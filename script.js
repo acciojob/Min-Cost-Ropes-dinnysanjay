ca00179410a009dad6fc9facb309ed55d6dfe0d1
dinnysanjay <160900894+dinnysanjay@users.noreply.github.com>
2024-04-14 18:23:30 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,8 +1,23 @@
-function mincost(arr)
-{ 
-//write your code here
-// return the min cost
-  
+function minCost(arr) {
+    let totalCost = 0;
+
+    // Sort the array in ascending order
+    arr.sort((a, b) => a - b);
+
+    // While there is more than one rope
+    while (arr.length > 1) {
+        // Take the two smallest ropes
+        let cost = arr.shift() + arr.shift();
+        
+        // Add their cost to the total cost
+        totalCost += cost;
+
+        // Insert the new rope in the correct position in the array
+        let index = arr.findIndex((length) => length >= cost);
+        arr.splice(index, 0, cost);
+    }
+
+    return totalCost;
 }
 
 module.exports=mincost;
